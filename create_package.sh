af67c00a79a931123181f247073673a016bcfcbc
phoityne.hs@gmail.com <phoityne.hs@gmail.com>
2025-07-12 07:30:08 +0900

    add actions.

--- create_package.sh
+++ create_package.sh
@@ -6,12 +6,12 @@
 version=$1
 
 dxt pack . download/pty-mcp-server-${version}.dxt
+cp -p download/pty-mcp-server-${version}.dxt download/pty-mcp-server-${version}.zip
 
 dxt sign --self-signed download/pty-mcp-server-${version}.dxt
 # dxt sign --cert cert.pem --key ca.key pty-mcp-server-${version}.dxt
 dxt info download/pty-mcp-server-${version}.dxt
 dxt verify download/pty-mcp-server-${version}.dxt
 
-cp -p download/pty-mcp-server-${version}.dxt download/pty-mcp-server-${version}.zip
 
 exit 0
