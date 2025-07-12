370621997ca2aa7492b7f0f04d0b1e3d0fc64cd8
phoityne.hs@gmail.com <phoityne.hs@gmail.com>
2025-07-12 20:58:18 +0900

    add

--- create_package.sh
+++ create_package.sh
@@ -5,6 +5,18 @@
 
 version=$1
 
+ls -la bin
+
+echo "Decompressing .xz binaries in bin/..."
+for file in bin/*.xz; do
+  [ -e "$file" ] || continue
+  echo "Decompressing $file"
+  xz -d -k "$file"
+done
+
+mv bin/pty-mcp-server-macos-aarch64 bin/pty-mcp-server
+mv bin/pty-mcp-server-windows-no-mingw.exe bin/pty-mcp-server.exe
+
 dxt pack . download/pty-mcp-server-${version}.dxt
 
 cp -p download/pty-mcp-server-${version}.dxt download/pty-mcp-server-${version}.zip
@@ -14,5 +26,6 @@ dxt sign --self-signed download/pty-mcp-server-${version}.dxt
 dxt info download/pty-mcp-server-${version}.dxt
 dxt verify download/pty-mcp-server-${version}.dxt
 
+ls -la download
 
 exit 0
